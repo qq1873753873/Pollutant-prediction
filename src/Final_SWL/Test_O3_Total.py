02eb7d7bbd475052eca68b4a2c3f7becb4fdb527
lihongfei <1873753873@qq.com>
2025-03-09 20:00:55 +0800

    图改成中文&&存预测值和真实值

--- src/Final_SWL/Test_O3_Total.py
+++ src/Final_SWL/Test_O3_Total.py
@@ -8,6 +8,7 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolu
 from keras.models import Sequential
 from keras.layers import Conv1D, MaxPooling1D, LSTM, Dense, Dropout, Flatten,Reshape
 from keras.layers import Input, Dense, Dropout, LayerNormalization, MultiHeadAttention, Add
+import matplotlib
 import matplotlib.pyplot as plt
 from datetime import datetime
 import seaborn as sns
@@ -15,6 +16,12 @@ import shap
 import tensorflow as tf
 from keras.models import Model
 tf.compat.v1.disable_eager_execution()
+from matplotlib.font_manager import FontProperties  
+zh_font = FontProperties(fname=".\\MSYH_Light_Regular.ttf", size=20)
+# 设置全局字体
+matplotlib.rcParams['font.family'] = zh_font.get_name()  # 设置字体族
+matplotlib.rcParams['font.sans-serif'] = [zh_font.get_name()]  # 设置无衬线字体
+matplotlib.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
 
 FLAG=3 #1 for CNN_LSTM, 2 for LSTM, 3 for Transformer
 
@@ -30,12 +37,17 @@ current_time = datetime.now().strftime("%y%m%d%H%M%S")
 path=f'.\\outputs\\{model_name}\\{current_time}\\'
 os.makedirs(path,exist_ok=True)
 res=""
+# # 设置全局字体大小
+# plt.rcParams['font.family'] = 'SimSun'
+# plt.rcParams.update({'font.size': 19})  # 设置全局字体大小
 
 # 读取并准备数据
 data = pd.read_excel('F:\\Code\\coagulant-forecast\\src\\swl_lstm\\data_O3.xlsx')
 origin_features = ['O3_Time', 'O3_Transported_Density', 'O3_Density', 'PH', 'UV254', 'TOC', 'ATZ', 'TMP', 'CBD', '2-AB', 'BZ','DEA', 'DIA', 'NTU', 'Conductivity']
 target =['UV254', 'TOC', 'ATZ', 'TMP', 'CBD', '2-AB', 'BZ','DEA', 'DIA']
-
+zh_target={feature: feature for feature in origin_features}
+zh_target.update({'O3_Time':'臭氧时间', 'O3_Transported_Density':'臭氧挡位', 'O3_Density':'臭氧浓度','Conductivity':'电导率'})
+print(zh_target)
 # 设置随机种子以确保结果可复现
 np.random.seed(42)
 # 为 'NTU' 和 'Conductivity' 列添加随机数
@@ -104,9 +116,12 @@ feature_importances[ntu_index] = 0.002  # 将 'NTU' 的重要性设为 0.01
 feature_importances[conductivity_index] = 0.008  # 将 'Conductivity' 的重要性设为 0.01
 # 将特征重要性和特征名称组合在一起
 feature_importance_df = pd.DataFrame({
-    'Feature': origin_features,
+    'Feature': [zh_target.get(target_name) for target_name in origin_features],
     'Importance': feature_importances
 })
+#res+=f"Feature importances: {feature_importance_df}\n"
+# 保存为 CSV 文件
+feature_importance_df.to_csv(path+'feature_importance.csv',encoding='utf-8-sig',index=False)
 # 按重要性从高到低排序
 feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
 # 打印排序后的特征重要性
@@ -115,13 +130,13 @@ print(feature_importance_df)
 # 可视化特征重要性
 plt.figure(figsize=(10, 6))
 plt.barh(feature_importance_df['Feature'], feature_importance_df['Importance'], color='skyblue')
-plt.xlabel("Feature Importance")
-plt.ylabel("Features")
-plt.title("Feature Importance (Sorted)")
+plt.xlabel("特征重要性")
+#plt.ylabel("特征")
+#plt.title("Feature Importance (Sorted)")
 plt.gca().invert_yaxis()  # 从高到低显示
 plt.tight_layout()  # 自动调整布局
 #plt.subplots_adjust(left=0.3)
-plt.savefig(path+"feature_importance_sorted.png")
+plt.savefig(path+"feature_importance_sorted.png",dpi=300)
 
 #相关性分析，筛掉了NTU和Conductivit
 features=['O3_Time', 'O3_Transported_Density', 'O3_Density', 'PH', 'UV254', 'TOC', 'ATZ', 'TMP', 'CBD', '2-AB', 'BZ','DEA', 'DIA']
@@ -259,23 +274,38 @@ for train_index, test_index in kf.split(X):
 
 
 # 转换为数组
-all_preds = np.array(all_preds)
-all_true = np.array(all_true)
+all_preds = np.array(all_preds)#135,9
+all_true = np.array(all_true)#135,9
 # 替换所有0点的预测值为真实值
-for j in range(0,136-15,15):
+time_length=all_preds.shape[1]
+for j in range(0,136-time_length,time_length):
     all_preds[j] = all_true[j]
+preds_df = pd.DataFrame(all_preds, columns=[f'预测_{zh_target.get(target[i])}' for i in range(all_preds.shape[1])])
+true_df = pd.DataFrame(all_true, columns=[f'真实_{zh_target.get(target[i])}' for i in range(all_true.shape[1])])
 
+# 合并两个 DataFrame
+result_df = pd.concat([true_df, preds_df], axis=1)
+# 生成时间索引
+time_index = [f'{i * 5}min' for i in range(time_length)]
+# 将时间索引添加到 DataFrame
+result_df.index = time_index*int(result_df.shape[0]/time_length)
+result_df.to_csv(path+'predictions_and_true_values.csv',encoding='utf-8-sig')
+#res+=f'真实值:形状{all_true.shape}\n+{all_true}\n预测值:形状{all_preds.shape}\n+{all_preds}\n'
 # 绘制所有目标变量的预测对比图
 for i, target_name in enumerate(target):
-    for j in range(0,136-15,15):
+    for j in range(0,136-time_length,time_length):
         plt.figure(figsize=(10, 6))
-        plt.plot(all_true[j:j+15, i], label=f'True {target_name}')
-        plt.plot(all_preds[j:j+15, i], label=f'Predicted {target_name}')
-        plt.title(f'{target_name} Prediction vs True Values across 5-folds')
-        plt.xlabel('Time Step')
-        plt.ylabel(target_name)
-        plt.legend()
-        plt.savefig(path + f'\\Total_{target_name}_prediction_vs_true_values_{round((j+1)/15)}.png')
+        plt.plot(all_true[j:j+time_length, i], label=f'真实值 {zh_target.get(target_name)}')
+        plt.plot(all_preds[j:j+time_length, i], label=f'预测值 {zh_target.get(target_name)}')
+        plt.title(f'{zh_target.get(target_name)} 预测值和真实值对比图')
+        plt.xlabel('臭氧反应时间(分钟)')
+        plt.ylabel(f'{zh_target.get(target_name)}浓度(ppb)')
+        # 添加图例，并去掉边框
+        plt.legend(frameon=False)
+        xticks = [tick for tick in plt.xticks()[0] if tick >= 0 and tick < time_length]
+        # 设置新的刻度标签为原刻度位置的 5 倍
+        plt.xticks(xticks, [f'{int(tick * 5)}' for tick in xticks])
+        plt.savefig(path + f'\\Total_{target_name}_prediction_vs_true_values_{round((j+1)/time_length)}.png',dpi=300)
 
 # 计算每个目标变量的平均指标
 for i, target_name in enumerate(target):
